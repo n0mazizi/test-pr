c03e8d3ef625e40bb18ab34c0c8ce96a05616776
dsbiodev <dsbiodev@gmail.com>
2023-08-31 10:19:36 +0900

    Set font for lbl1 in ui.py

--- ui.py
+++ ui.py
@@ -2,8 +2,10 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
                              QLineEdit, QComboBox, QLabel) # QLabel 추가
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont #QFont 추가
 from PyQt5 import QtCore #모듈 추가
+
+
 
 class View(QWidget):
 
@@ -17,6 +19,7 @@ class View(QWidget):
         self.te1.setReadOnly(True)
 
         self.lbl1=QLabel('v2.3.0', self) #버전 정보 표시를 위한 lbl1 위젯 생성
+        self.lbl1.setFont(QFont('Consolas', 10)) # 폰트 설정 추가, Consolas, 사이즈 10
 
         self.btn1=QPushButton('Calc', self) #버튼 이름 변경
         self.btn2=QPushButton('Clear', self)
